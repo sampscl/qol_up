ab6f8d8d4659f370841e4dc370eebf0b569fa82e
Clay Sampson <pdgeek@gmail.com>
2021-09-04 08:41:39 -0400

    more stable tests, esp on macos

--- spec/yaml_config_file_spec.exs
+++ spec/yaml_config_file_spec.exs
@@ -12,14 +12,18 @@ defmodule YamlConfigFileSpec do
       # make an empty temporary config file
       cfg_file = temp_config_file("")
       # note: it always parses config files on startup
-      {:ok, pid} = QolUp.YamlConfigFile.start_link(config_file: cfg_file, name: __ENV__.function |> then(&(elem(&1, 0))))
+      name = __ENV__.function |> then(&(elem(&1, 0)))
+      {:ok, pid} = QolUp.YamlConfigFile.start_link(config_file: cfg_file, name: name)
       assert(pid |> to(be_pid()))
+      GenServer.stop(name)
     end
 
     it "parses valid config files" do
       cfg_file = temp_config_file("---\nfoo: bar")
-      {:ok, pid} = QolUp.YamlConfigFile.start_link(config_file: cfg_file, name: __ENV__.function |> then(&(elem(&1, 0))))
+      name = __ENV__.function |> then(&(elem(&1, 0)))
+      {:ok, pid} = QolUp.YamlConfigFile.start_link(config_file: cfg_file, name: name)
       assert(pid |> to(be_pid()))
+      GenServer.stop(name)
     end
 
     it "reads config parameters" do
@@ -28,6 +32,7 @@ defmodule YamlConfigFileSpec do
       {:ok, pid} = QolUp.YamlConfigFile.start_link(config_file: cfg_file, name: name)
       assert(pid |> to(be_pid()))
       expect(QolUp.YamlConfigFile.get_item!(name, "foo") |> to(eq("bar")))
+      GenServer.stop(name)
     end
 
     it "can start monitoring the filesystem" do
@@ -36,6 +41,7 @@ defmodule YamlConfigFileSpec do
       {:ok, pid} = QolUp.YamlConfigFile.start_link(config_file: cfg_file, name: name, watch_fs: true)
       assert(pid |> to(be_pid()))
       expect(QolUp.YamlConfigFile.get_item!(name, "foo") |> to(eq("bar")))
+      GenServer.stop(name)
     end
 
     it "reloads configs when monitoring" do
@@ -47,9 +53,9 @@ defmodule YamlConfigFileSpec do
 
       File.write!(cfg_file, "---\nfoo: baz")
       # Give a bit for change / notify system to process the change
-      Process.sleep(1_5000)
+      Process.sleep(1_500)
       expect(QolUp.YamlConfigFile.get_item!(name, "foo") |> to(eq("baz")))
-
+      GenServer.stop(name)
     end
   end
 end
